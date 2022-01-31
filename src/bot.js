ac032205b5237c9dd4f5863c5b52027e10c2671f
KrautByte <lars@krautbyte.com>
2022-01-31 05:53:44 +0100

    command change

--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,16 @@
-const { Client, Intents } = require('discord.js');
-const { token, filterList } = require('../config.json');
+const {Client, Intents} = require('discord.js');
+const {token} = require('../config.json');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] });
+const client = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS]});
 
-const res = "### Please don't ask to ask, just ask ###\n" +
-    "https://dontasktoask.com/\n" +
-    "https://xyproblem.info/\n"+
-    "https://stackoverflow.com/help/how-to-ask\n" +
-    "http://catb.org/~esr/faqs/smart-questions.html\n"+
-    "https://www.nohello.com/"
+const res = "### Please don't ask to ask, just ask ###\n" + "https://dontasktoask.com/\n" + "https://xyproblem.info/\n" + "https://stackoverflow.com/help/how-to-ask\n" + "http://catb.org/~esr/faqs/smart-questions.html\n" + "https://www.nohello.com/"
 
 client.on('ready', c => {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
-    if(message.content.includes(".d") || message.content.includes(".data")) message.channel.send(res)
+    if (message.content == ".d" || ".data") message.channel.send(res)
 });
 
 client.login(token);
